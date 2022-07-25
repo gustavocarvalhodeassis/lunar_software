83cd55221e1237a2bb42c5ea56ed7f12fd05955c
Gustavo Carvalho De Assis <44401808+gustavocarvalhodeassis@users.noreply.github.com>
2022-07-25 17:11:57 -0300

    commit

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,15 +1,77 @@
 import './style.css'
+import Logo from '../logo/logo'
+import { Link } from 'react-router-dom'
+import { navbarData } from '../navbar/data/navbarData'
+import { socialData } from '../../data/socialIconData'
 
 const Footer = () => {
-     return ( 
+     return (
           <footer className="footer">
                <div className='container'>
                     <div className='footer-content'>
-
+                         <div className='footer-content-cl'>
+                              <Logo />
+                              <p className="content-p">
+                                   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                              </p>
+                              <div className="social-list">
+                                   {
+                                        socialData.map((item, index) => {
+                                             return (
+                                                  <div key={index} className="social-icon">
+                                                       {item.icon}
+                                                  </div>
+                                             )
+                                        })
+                                   }
+                              </div>
+                         </div>
+                         <div className='footer-content-cl links'>
+                              <Link to='/'>
+                                   <button>
+                                        <li>
+                                             Inicio
+                                        </li>
+                                   </button>
+                              </Link>
+                              <a href='#about'>
+                                   <button>
+                                        <li>
+                                             Sobre
+                                        </li>
+                                   </button>
+                              </a>
+                              <Link to='/'>
+                                   <button>
+                                        <li>
+                                             Painel do Representante
+                                        </li>
+                                   </button>
+                              </Link>
+                              <Link to='/'>
+                                   <button>
+                                        <li>
+                                             Painel do contador
+                                        </li>
+                                   </button>
+                              </Link>
+                              <Link to='/'>
+                                   <button>
+                                        <li>
+                                             Painel do suporte
+                                        </li>
+                                   </button>
+                              </Link>
+                              <Link to='/integrar'>
+                                   <button className='btn filled-btn'>
+                                        Integrar!
+                                   </button>
+                              </Link>
+                         </div>
                     </div>
                </div>
           </footer>
-      );
+     );
 }
- 
+
 export default Footer;
--- src/router.jsx
+++ src/router.jsx
@@ -5,6 +5,7 @@ import IntegrateChose from "./screens/integrar/integrateChose";
 import IntegrateRepresentante from "./screens/integrar/integrateRepresentante";
 import NavBar from "./components/navbar/navbar";
 import IntegrateCliente from "./screens/integrar/integrateCliente";
+import Footer from "./components/footer/footer";
 
 const Router = () => {
     return (
@@ -16,6 +17,7 @@ const Router = () => {
                 <Route element={<IntegrateRepresentante />} path={'/integrar/representante'} />
                 <Route element={<IntegrateCliente />} path={'/integrar/cliente'} />
             </Routes>
+            <Footer />
         </BrowserRouter>
     );
 }
